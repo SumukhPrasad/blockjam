4400e2f6cb358284ad09d05f2a471b29e4fc5923
Sumukh Prasad <sumukhprasad@icloud.com>
2023-09-18 16:44:28 +0530

    Added blockly.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,9 @@
 class QuestionsController < ApplicationController
      before_action :authenticate_user!
+
      def show
+          
+          ActiveStorage::Current.host = request.base_url
           @heat = Heat.find_by(:slug => params[:heat_slug])
           if @heat == nil
                redirect_to root_path, flash: {alert: "Heat does not exist."}
