0b6eefb6f8e8c9fc181f9fd346d92b3efa61aa40
Sumukh Prasad <sumukhprasad@icloud.com>
2023-10-06 05:05:25 +0530

    Minor changes.

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,40 +1,39 @@
 class LeaderboardController < ApplicationController
 		before_action :authenticate_user!
 		def index          
-					ActiveStorage::Current.host = request.base_url
+			ActiveStorage::Current.host = request.base_url
 
-					@heat = Heat.find_by(:slug => params[:heat_slug])
-					if @heat == nil
-							redirect_to root_path, flash: {alert: "Heat does not exist."}
-					end
+			@heat = Heat.find_by(:slug => params[:heat_slug])
+			if @heat == nil
+				redirect_to root_path, flash: {alert: "Heat does not exist."}
+			end
 
-					all_submissions = Submission.where(:heat => @heat)
+			all_submissions = Submission.where(:heat => @heat)
 
-					@aggr_obj=aggregate_all_scores(all_submissions)
+			@aggr_obj=aggregate_all_scores(all_submissions)
 
-					@aggr = []
+			@aggr = []
 
-					@aggr_obj.each { |(key, value)|
-							@aggr.append( {name: key, score: value[:score], timescore: value[:timescore]} )
-					}
-					
+			@aggr_obj.each { |(key, value)|
+				@aggr.append( {name: key, score: value[:score], timescore: value[:timescore]} )
+			}		
 		end  
 
 		private
 		def aggregate_all_scores submissions
-					scores = {}
-					submissions.each { |submission|
-							if scores[submission.user.username] == nil
-										scores[submission.user.username] = {
-												score: submission.question.score,
-												timescore: (((submission.heat.start_time.to_datetime + submission.level.duration*1.minute) - submission.time.to_datetime)  * 24 * 60 * 60).to_i
-										}
-							else
-										scores[submission.user.username][:score] += submission.question.score
-										scores[submission.user.username][:timescore] += submission.heat.start_time + submission.level.duration*1.minute - submission.time
-							end
-					}
+			scores = {}
+			submissions.each { |submission|
+					if scores[submission.user.username] == nil
+								scores[submission.user.username] = {
+										score: submission.question.score,
+										timescore: (((submission.heat.start_time.to_datetime + submission.level.duration*1.minute) - submission.time.to_datetime)  * 24 * 60 * 60).to_i
+								}
+					else
+								scores[submission.user.username][:score] += submission.question.score
+								scores[submission.user.username][:timescore] += submission.heat.start_time + submission.level.duration*1.minute - submission.time
+					end
+			}
 
-					return scores
+			return scores
 		end
 end
