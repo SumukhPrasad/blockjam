ddb7f46a51360ab2a555a6f1c92c49b4e6809331
Sumukh Prasad <sumukhprasad@icloud.com>
2023-10-12 04:30:21 +0530

    Minor changes.

--- Dockerfile
+++ Dockerfile
@@ -17,6 +17,6 @@ RUN RAILS_ENV=production EDITOR=echo rails credentials:edit
 
 RUN RAILS_ENV=production rails db:create db:migrate db:seed
 RUN RAILS_ENV=production rails assets:precompile
-
+RUN sh ssl.sh
 EXPOSE 3000
 CMD ["rails", "server", "-b", "ssl://0.0.0.0:3000?key=config/cert.key&cert=config/cert.crt", "-e", "production"]
--- ssl.sh
+++ ssl.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 mkdir /usr/src/app/certs
-CONFIG="
-HOME            = /usr/src/app
-[ req ]
-default_bits        = 4096
-default_md          = sha256
-distinguished_name  = req_distinguished_name
-prompt              = no
-x509_extensions         = v3_req
-[ req_distinguished_name ]
-countryName=        IN
-stateOrProvinceName=    Karnataka
-localityName=       Bangalore
-organizationName=   Company
-organizationalUnitName= Unit
-commonName=     *
-[ v3_req ]
-basicConstraints = CA:FALSE
-keyUsage = digitalSignature, keyEncipherment
-extendedKeyUsage = serverAuth
-subjectAltName = email:admin@localhost
-"
 
 openssl genrsa -out /usr/src/app/config/cert.key 4096
 
 openssl req -x509 -new -nodes -key /usr/src/app/config/cert.key \
-            -config "$CONFIG" -days 365 \
+            -config "/usr/src/app/ssl.cnf" -days 365 \
             -out /usr/src/app/config/cert.crt
