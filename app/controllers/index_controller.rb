c780d055f57ed4590eb27993a929a6e633e27f20
Sumukh Prasad <sumukhprasad@icloud.com>
2023-10-17 14:14:27 +0530

    Minor changes.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,9 @@ class IndexController < ApplicationController
 		def index
 					if params[:slug] != nil
 							@heat = Heat.find_by(:slug => params[:slug])
-							if @heat != nil
+							if @heat == nil
+								redirect_to root_path, flash: {alert: "Heat does not exist."}
+							else
 										redirect_to heat_path(@heat.slug)
 							end
 					end
