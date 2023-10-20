fa6cead6671472a553a18b2a8218bdb5cf9d8d6a
Sumukh Prasad <sumukhprasad@icloud.com>
2023-10-20 17:45:43 +0530

    Linted code.

--- db/migrate/20231019120253_add_unique_constraint_to_heats.rb
+++ db/migrate/20231019120253_add_unique_constraint_to_heats.rb
@@ -1,5 +1,5 @@
 class AddUniqueConstraintToHeats < ActiveRecord::Migration[7.0]
-  def change
-    add_index :heats, [:slug], unique: true, name: 'uniq_slug_per_heat'
-  end
+	def change
+		add_index :heats, [:slug], unique: true, name: 'uniq_slug_per_heat'
+	end
 end
